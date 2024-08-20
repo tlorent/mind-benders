1e079e48b39f5d4fcb66d31dfc83b4ac88978ad6
bteicho@gmail.com <btegicho@gmail.com>
2024-08-21 00:31:36 +0200

    restart button is working, score is not holding

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -7,6 +7,9 @@ import { createQuestionElement } from '../views/questionView.js';
 import { createAnswerElement } from '../views/answerView.js';
 import { quizData } from '../data.js';
 
+let score = 0;
+// let selected= null;
+
 export const initQuestionPage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
   userInterface.innerHTML = '';
@@ -19,8 +22,12 @@ export const initQuestionPage = () => {
 
   const answersListElement = document.getElementById(ANSWERS_LIST_ID);
 
+  answersListElement.innerHTML = ''; // clear the answers list previously answered
+  // render each answer
+
   for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
     const answerElement = createAnswerElement(key, answerText);
+
     answersListElement.appendChild(answerElement);
   }
 
@@ -29,8 +36,50 @@ export const initQuestionPage = () => {
     .addEventListener('click', nextQuestion);
 };
 
+// expermenting with the handleAnswerClick function
+
+const handleAnswerClick = (key) => {
+  const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+  currentQuestion.selected = key;
+  if (currentQuestion.selected === questions.correct) {
+    score = score + 1;
+  }
+  //console.log(score);
+}
+
+// export const nextQuestion = () => {
+//   quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
+
+//   if (quizData.currentQuestionIndex < quizData.questions.length) {
+//     initQuestionPage();
+//   } else {
+//     showFinalScore();
+//   }
+//   // initQuestionPage();
+// }
+
 const nextQuestion = () => {
   quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
 
-  initQuestionPage();
+  if (quizData.currentQuestionIndex < quizData.questions.length) {
+    initQuestionPage();
+  } else {
+    showFinalScore();
+  }
+  // initQuestionPage();
 };
+
+const showFinalScore = () => {
+  const userInterface = document.getElementById(USER_INTERFACE_ID);
+  userInterface.innerHTML = `<h1>Your final score is ${score}</h1>
+  <button id="restart-button">Restart</button>`;
+  document.getElementById('restart-button').addEventListener('click', () => {
+    quizData.currentQuestionIndex = 0;
+    score = 0;
+    initQuestionPage();
+  });
+
+  // const finalScoreElement = document.createElement('h1');
+  // finalScoreElement.innerText = `Your final score is ${score}`;
+  // userInterface.appendChild(finalScoreElement);
+}
--- src/views/answerView.js
+++ src/views/answerView.js
@@ -4,8 +4,20 @@
  */
 export const createAnswerElement = (key, answerText) => {
   const element = document.createElement('li');
+  element.classList.add('answer-option'); // add a class for styling
   element.innerHTML = String.raw`
     ${key}: ${answerText};
   `;
+  
+  
+  // add event listener to clicked answer
+  element.addEventListener('click',() => handleAnswerClick(key));
   return element;
 };
+
+ // export answer element to be used in questionPage.js
+
+
+
+
+ 
