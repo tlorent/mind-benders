e344dd94f1f25870c4843e102b7c1341b06a17e7
DESKTOP-1N0FNIS\elias <eliasrajha7@gmail.com>
2024-08-24 20:59:48 +0200

    asking for the username

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -1,4 +1,5 @@
-import { START_QUIZ_BUTTON_ID } from '../constants.js';
+import { START_QUIZ_BUTTON_ID, USERNAME_INPUT_ID } from '../constants.js';
+import { quizData } from '../data.js';
 
 /**
  * Create the welcome screen
@@ -7,7 +8,7 @@ import { START_QUIZ_BUTTON_ID } from '../constants.js';
 export const createWelcomeElement = () => {
   const element = document.createElement('div');
   element.innerHTML = String.raw`
-    <h1>Welcome</h1>
+    <h1>Welcome, <input type="text" id="${USERNAME_INPUT_ID}" value="" placeholder=" What is your name?"></h1>
     <button id="${START_QUIZ_BUTTON_ID}">start quiz</button>
   `;
   return element;
